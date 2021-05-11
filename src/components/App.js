10900d695c532f1fe28b7a11855f30267ba3e1bb
youyou4691 <82370504+youyou4691@users.noreply.github.com>
2021-05-11 23:11:36 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
 
